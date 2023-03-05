98dac69092f9245efc51f160e8ad962a17f879fd
krch <krch@Ubuntu-22.04.myguest.virtualbox.org>
2023-03-05 16:12:24 +0300

    Add first task

--- Exercises/1-random.js
+++ Exercises/1-random.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const random = (min, max) => {
-  // Generate random Number between from min to max
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+const random = (...args) => {
+  const [ min, max ] = args.length == 1 ? [ 0, args[0] ] : [ args[0], args[1] ];
+  return Math.random()*(max - min) + min;
 };
 
 module.exports = { random };
