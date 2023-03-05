b213919416c833225e7d833d5db0add554d3b634
krch <krch@Ubuntu-22.04.myguest.virtualbox.org>
2023-03-05 18:12:44 +0300

    Fix eslint erors

--- Exercises/1-random.js
+++ Exercises/1-random.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const random = (...args) => {
-  const [ min, max ] = args.length == 1 ? [ 0, args[0] ] : [ args[0], args[1] ];
-  return Math.random()*(max - min) + min;
+  const [ min, max ] = args.length > 1 ? [ args[0], args[1] ] : [ 0, args[0] ];
+  return Math.random() * (max - min) + min;
 };
 
 module.exports = { random };
